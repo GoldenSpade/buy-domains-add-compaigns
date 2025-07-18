429d881c32b6e6f23b8793a4e2298ee722de6aa0
GoldenSpade <vladislav.sviridov.dev@gmail.com>
2025-07-18 13:43:47 +0300

    fix

--- src/stores/domainStore.js
+++ src/stores/domainStore.js
@@ -2,10 +2,8 @@ import { defineStore } from 'pinia'
 import { ref, watch } from 'vue'
 
 export const useDomainStore = defineStore('domain', () => {
-  // const domains = ref(loadDomainsFromLocalStorage())
-  const domains = ref([
-    { message: '', name: 'sperm-donation-123.click', status: 'pending' },
-  ])
+  const domains = ref(loadDomainsFromLocalStorage())
+
   const selectedSedoAccount = ref('TT1')
   // Добавить домен
   function addDomain(name) {
